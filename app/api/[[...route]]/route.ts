b0c919bd6c006688cb0ea198ec4050a50aa7bd44
Giovanni <giovanni.chitarrelli@gmail.com>
2024-06-18 13:31:41 -0300

    feat: add insert accounts

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -1,11 +1,11 @@
 import { Hono } from "hono";
 import { handle } from "hono/vercel";
 import accounts from "./accounts";
- 
 
-export const runtime = "edge";
+ export const runtime = "edge";
 const app = new Hono().basePath("/api");
 
+ 
 const routes = app.route("/accounts", accounts);
 
 export const GET = handle(app);
